f7928f2c0d5a74d102f36a59af1c70cde0f9e39c
athreyc <achandr8@ncsu.edu>
2015-02-01 17:37:45 -0500

    Custom header for constants

---
+++ merge.h
@@ -0,0 +1,34 @@
+/*
+ *
+ * Name: Arun Athrey Chandrasekaran
+ *
+ */
+
+
+#ifndef MERGE_H
+#define MERGE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<limits.h>
+#include <sys/time.h>
+
+
+#define DEBUG 0
+
+#define IN_BUFF_SIZE 1000
+#define OUT_BUFF_SIZE 1000
+
+#define SUPER_RUN_NO 15
+
+FILE *index_fp;
+int in_buf[IN_BUFF_SIZE];
+int out_buf[OUT_BUFF_SIZE];
+char i_filename[100];
+char o_filename[100];
+
+
+#endif
+
+                
